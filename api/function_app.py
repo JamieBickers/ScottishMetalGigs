9292743b9464c7c62acb5a15286e306de7294ef1
Jamie Bickers <jamie.bickers@metaswitch.com>
2023-10-01 22:07:39 +0100

    pipeline

--- api/function_app.py
+++ api/function_app.py
@@ -9,7 +9,13 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def getGigs(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    return func.HttpResponse(
-        get_gigs(),
-        mimetype="application/json",
-    )
+    try:
+        return func.HttpResponse(
+            get_gigs(),
+            mimetype="application/json",
+        )
+    except Exception as e:
+        return func.HttpResponse(
+            str(e),
+            mimetype="application/json",
+        )
