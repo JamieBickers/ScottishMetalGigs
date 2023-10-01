0269872bbbf50b24f49aa0e5c5e43dc7b902e50f
Jamie Bickers <jamie.bickers@metaswitch.com>
2023-10-01 23:40:03 +0100

    test

--- api/genre.py
+++ api/genre.py
@@ -14,28 +14,33 @@ def get_artist_genre(artist):
         "format": "json"
     }
 
-    try:
-        logging.error("Trying request to: " + base_url)
-        #logging.error("Params: " + str(params))
-        #response = requests.get(base_url, params=params)
-        #requests.get("http://www.google.com")
-        req = requests.Request('GET', 'https://httpbin.org/get')
-        prepared = req.prepare()
-        s = requests.Session()
-        logging.error(req)
-        resp = s.send(prepared)
-        logging.error(prepared)
-        logging.error("Response code: " + str(resp.status_code))
-        logging.error("Response: " + str(resp))
-    except Exception as e:
-        logging.error("Exception: " + str(e))
-    # data = response.json()
+    session = requests.Session()
+    request = requests.Request("GET", base_url, params=params)
+    prepared = request.prepare()
+    response = session.send(prepared)
+    data = response.json()
+
+    # try:
+    #     logging.error("Trying request to: " + base_url)
+    #     #logging.error("Params: " + str(params))
+    #     #response = requests.get(base_url, params=params)
+    #     #requests.get("http://www.google.com")
+    #     req = requests.Request('GET', 'https://httpbin.org/get')
+    #     prepared = req.prepare()
+    #     s = requests.Session()
+    #     logging.error(req)
+    #     resp = s.send(prepared)
+    #     logging.error(prepared)
+    #     logging.error("Response code: " + str(resp.status_code))
+    #     logging.error("Response: " + str(resp))
+    # except Exception as e:
+    #     logging.error("Exception: " + str(e))
     
     genres = []
-    # if response.status_code == 200 and "artist" in data and "tags" in data["artist"]:
-    #     genres = [tag["name"] for tag in data["artist"]["tags"]["tag"]]
-    # else:
-    #     return []
+    if response.status_code == 200 and "artist" in data and "tags" in data["artist"]:
+        genres = [tag["name"] for tag in data["artist"]["tags"]["tag"]]
+    else:
+        return []
     
     return genres
 
