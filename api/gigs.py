99da4ed4922bd6be27292c3641e7fcc368e48d66
Jamie Bickers <jamie.bickers@metaswitch.com>
2023-10-02 20:35:47 +0100

    fix

--- api/gigs.py
+++ api/gigs.py
@@ -154,4 +154,5 @@ def get_new_gigs():
 def get_existing_gigs():
     repository = Repository()
     existing_gigs = repository.get_gigs()
-    return json.dumps([gig_from_db_to_gig(gig).as_serialisable() for gig in existing_gigs])
+    gigs = [gig_from_db_to_gig(gig) for gig in existing_gigs if not is_gig_in_past(gig)]
+    return json.dumps([gig.as_serialisable() for gig in gigs])
